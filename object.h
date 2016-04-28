b5ad690853ce69d079f84452f6597c0cc61faa4c
Carter Turnbaugh <carterturn@tutanota.de>
2016-04-28 12:53:26 -0700

    Made appropriate functions virtual

--- object.h
+++ object.h
@@ -1,15 +1,17 @@
 #pragma once
 
 #include <vector>
+#include "material.h"
+#include "tsvector.h"
 
 class object : public material{
 public:
 
 
-	std::vector<tsvector> get_points(mpf_class density);
+	virtual std::vector<tsvector> get_points(mpf_class density) = 0;
 
 	virtual tsvector get_reflected(int point) = 0;
 	virtual tsvector get_transmitted(int point) = 0;
 
 	virtual bool inside(tsvector point) = 0;
-}
+};
