0c5fdbb1c0ed0cc1453fb81c5368c659c6a9c9fa
Carter Turnbaugh <carterturn@tutanota.de>
2016-04-28 15:26:02 -0700

    Added tau

--- clock.cpp
+++ clock.cpp
@@ -1,6 +1,7 @@
 #include "clock.h"
+#include "constant.h"
 
 mpf_class clock::get_angle(mpf_class wavelength, mpf_class distance){
 	// tau = 1 wavelength
-	return tau * ((distance - floor(distance / wavelength)) / wavelength);
+	return cnst::tau * ((distance - floor(distance / wavelength)) / wavelength);
 }
--- constant.h
+++ constant.h
@@ -10,6 +10,9 @@ namespace cnst{
 	// All the physics constants
 	extern mpf_class c;
 
+	// All the math constants
+	extern mpf_class tau;
+
 	// Simulation specific constants
 	extern mpf_class precision;
 };
