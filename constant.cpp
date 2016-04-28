6006d3a9ea05a3ae8c559fbf442f5dfc398f2f70
Carter Turnbaugh <carterturn@tutanota.de>
2016-04-27 22:12:59 -0700

    Fundamental constants!

---
+++ constant.cpp
@@ -0,0 +1,5 @@
+#include "constant.h"
+
+cnst::tau = 6.2831853071_mpf;
+cnst::c = 299792458_mpf;
+cnst::precision = 1.0_mpf;
---
+++ constant.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <gmpxx.h>
+
+/**
+   System constants
+*/
+
+namespace cnst{
+	// All the physics constants
+	extern mpf_class c;
+
+	// Simulation specific constants
+	extern mpf_class precision;
+};
