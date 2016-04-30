88124036337fca75dfebc926863a24010d214c76
Carter Turnbaugh <carterturn@tutanota.de>
2016-04-29 22:49:19 -0700

    Block test

---
+++ test_block.cpp
@@ -0,0 +1,64 @@
+#include "block.h"
+#include <iostream>
+
+#ifdef GRAPHICS
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <unistd.h>
+#endif
+
+using namespace std;
+
+int main(int argc, char * argv[]){
+
+#ifdef GRAPHICS
+	glfwInit();
+	GLFWwindow * window = glfwCreateWindow(1000, 1000, "Util Test", NULL, NULL);
+
+	if(!window){
+		cout << "Window creation failed\n";
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glOrtho(-10, 10, -10, 10, -10, 10);
+#endif
+
+	block b = block(tsvector(), tsvector(1, 5, 1), tsvector(1.57, 0, 0));
+
+	cout << b.inside(tsvector(0.5, 2, 0)) << "\n";
+	cout << b.inside(tsvector(1, 2.5, 0)) << "\n";
+	cout << b.inside(tsvector(0.5, 2.5, 0)) << "\n";
+	cout << b.inside(tsvector(0.5, 0, 0)) << "\n";
+	cout << b.inside(tsvector(0, 2.5, 0.5)) << "\n";
+
+#ifdef GRAPHICS
+	while(true){
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_LINES);
+
+		glColor3f(0.5f, 0.5f, 0.5f);
+		glVertex3f(-10, 0, 0);
+		glVertex3f(10, 0, 0);
+		glVertex3f(0, -10, 0);
+		glVertex3f(0, 10, 0);
+		glVertex3f(0, 0, -10);
+		glVertex3f(0, 0, 10);
+
+		glEnd();
+
+		glColor3f(0.0f, 1.0f, 0.0f);
+	
+		b.draw();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		usleep(50000);
+	}
+
+	glfwTerminate();
+#endif
+
+	return 0;
+}
