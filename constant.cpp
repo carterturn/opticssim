7bc3f14770a39f85f0ed86f47cb4c0ae41a2acfa
Carter Turnbaugh <carterturn@tutanota.de>
2016-04-30 22:18:30 -0700

    Added constants for 3Difying, fixed CMakeLists

--- constant.cpp
+++ constant.cpp
@@ -1,5 +1,5 @@
 #include "constant.h"
 
-cnst::tau = 6.2831853071_mpf;
-cnst::c = 299792458_mpf;
-cnst::precision = 1.0_mpf;
+mpf_class cnst::tau = 6.2831853071_mpf;
+mpf_class cnst::c = 299792458_mpf;
+mpf_class cnst::precision = 1.0_mpf;
