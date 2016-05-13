ab28897eb22e50109d00d0a03918e84d993856c6
Carter Turnbaugh <carterturn@tutanota.de>
2016-05-13 15:28:15 -0700

    Added getter function for wavelength of light

--- light.h
+++ light.h
@@ -11,6 +11,10 @@ light(tsvector origin, mpf_class wavelength) : origin(origin), wavelength(wavele
 	// Determines the path of the light
 	virtual int calculate(std::vector<object*> objects, mpf_class density, int depth) = 0;
 
+	mpf_class get_wavelength(){
+		return wavelength;
+	}
+
 #ifdef GRAPHICS
 	// Draws the light (however the thing wants)
 	virtual void draw() = 0;
