52c11518e959d64644a86b6b72268d10bd9de39d
Yoni Lerner <yonilern@gmail.com>
2016-06-01 21:58:06 -0700

    Hopefully better?

--- concavespheremirror.h
+++ concavespheremirror.h
@@ -6,8 +6,16 @@
 class concavespheremirror : public concavesphere, public mirror {
 public:
 concavespheremirror(tsvector center, mpf_class radius, mpf_class height, tsvector rot) : concavesphere(center, radius, height, rot), mirror() {}
-	std::vector<tsvector> get_points(mpf_class density);	
-	tsvector get_surface_normal(tsvector point);
-	tsvector get_intersection(photon* incident_photon);
-	photon* get_redirected_photon(photon* incident_photon);
+	std::vector<tsvector> get_points(mpf_class density){
+		return concavesphere::get_points(density);
+	}
+	tsvector get_surface_normal(tsvector point){
+		return concavesphere::get_surface_normal(point);
+	}
+	tsvector get_intersection(photon* incident_photon){
+		return concavesphere::get_intersection(incident_photon);
+	}
+	photon* get_redirected_photon(photon* incident_photon){
+		return mirror::get_redirected_photon(incident_photon);
+	}
 };
--- convexspheremirror.h
+++ convexspheremirror.h
@@ -6,8 +6,16 @@
 class convexspheremirror : public convexsphere, public mirror {
 public:
 	convexspheremirror(tsvector center, mpf_class radius, mpf_class height, tsvector rot) : convexsphere(center, radius, height, rot), mirror() {}
-	std::vector<tsvector> get_points(mpf_class density);	
-	tsvector get_surface_normal(tsvector point);
-	tsvector get_intersection(photon* incident_photon);
-	photon* get_redirected_photon(photon* incident_photon);
+	std::vector<tsvector> get_points(mpf_class density){
+		return convexsphere::get_points(density);
+	}
+	tsvector get_surface_normal(tsvector point){
+		return convexsphere::get_surface_normal(point);
+	}
+	tsvector get_intersection(photon* incident_photon){
+		return convexsphere::get_intersection(incident_photon);
+	}
+	photon* get_redirected_photon(photon* incident_photon){
+		return mirror::get_redirected_photon(incident_photon);
+	}
 };
